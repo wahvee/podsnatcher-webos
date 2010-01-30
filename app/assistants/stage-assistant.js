36151e090fa948d685146c8e5d8424f92ae25529
Ryan Lovelett <ryan@wahvee.com>
2010-01-30 21:54:05 +0000

    Removed objects

--- app/assistants/stage-assistant.js
+++ app/assistants/stage-assistant.js
@@ -4,9 +4,10 @@ function StageAssistant() {
 StageAssistant.prototype.setup = function() {
 	try {
 		Mojo.Log.info("[Prototype] %s", Prototype.Version);
+		Mojo.Log.info("[jQuery] %s", $().jquery);
 	} catch(version_error) {
-		Mojo.Log.error("[Prototype] %s", version_error.message);
-	}	
+		Mojo.Log.error("[Prototype or jQuery Error] %s", version_error.message);
+	}
 	
 	this.controller.pushScene({name: "main", disableSceneScroller: true});
 	this.controller.setWindowOrientation("free");
