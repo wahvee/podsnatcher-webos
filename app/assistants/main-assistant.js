01500ec9dddcb90620c9b8152008f275d6ef9bc5
Ryan Lovelett <ryan@wahvee.com>
2010-01-30 22:26:30 +0000

    Test...
    And new Podcast class.

--- app/assistants/main-assistant.js
+++ app/assistants/main-assistant.js
@@ -1,11 +1,20 @@
 function MainAssistant(argFromPusher) {
-}
+	// Get the list on the screen
+	this.podcastList_ctrl = this.controller.get('podcastList');
+	this.podcast = new Podcast('http://www.wdwradio.com/xml/wdwradio.xml');
+	Mojo.Log.logProperties(this.podcast);
+};
 
 MainAssistant.prototype = {
 	setup: function() {
 		Ares.setupSceneAssistant(this);
+		this.LoadPodcasts();
 	},
 	cleanup: function() {
 		Ares.cleanupSceneAssistant(this);
-	}
+	}	
+};
+
+MainAssistant.prototype.LoadPodcasts = function() {
+	
 };
---
+++ app/lib/Podcasts.js
@@ -0,0 +1,9 @@
+var Podcast = Class.create(JFeed, {
+	initialize: function(feedURL) {
+		this.url = feedURL;
+	}
+});
+
+Podcast.prototype.UpdateFeed = function() {
+	
+};
