d2004b41002ed350deac32ae9574c7c29969f36a
Ryan Lovelett <ryan@wahvee.com>
2010-01-31 21:55:48 +0000

    Removed Main

--- src/app/assistants/stage-assistant.js
+++ src/app/assistants/stage-assistant.js
@@ -8,6 +8,8 @@ StageAssistant.prototype.setup = function() {
 		Mojo.Log.error("[Prototype] %s", version_error.message);
 	}
 	
-	this.controller.pushScene({name: "main", disableSceneScroller: true});
-	this.controller.setWindowOrientation("free");
+	this.controller.pushScene({name: "stage", disableSceneScroller: true});
+	//this.controller.setWindowOrientation("free");
+	//'http://www.wdwradio.com/xml/wdwradio.xml'
+	//'http://revision3.com/diggnation/feed/MP3'
 };
