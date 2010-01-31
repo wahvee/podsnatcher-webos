2ca4f6e0e2e3e3d2e9e225e9037a127da1bb8ccc
Ryan Lovelett <ryan@wahvee.com>
2010-01-31 22:22:59 +0000

    Removed all ares stuff
    Removed ares stuff

--- src/app/assistants/stage-assistant.js
+++ src/app/assistants/stage-assistant.js
@@ -8,7 +8,7 @@ StageAssistant.prototype.setup = function() {
 		Mojo.Log.error("[Prototype] %s", version_error.message);
 	}
 	
-	this.controller.pushScene({name: "stage", disableSceneScroller: true});
+	this.controller.pushScene({name: "player", disableSceneScroller: true});
 	//this.controller.setWindowOrientation("free");
 	//'http://www.wdwradio.com/xml/wdwradio.xml'
 	//'http://revision3.com/diggnation/feed/MP3'
