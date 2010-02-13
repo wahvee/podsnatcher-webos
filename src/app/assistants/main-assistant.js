cef15f075bcb4510ecb5a4227c00c1724110c780
Ryan Lovelett <ryan@wahvee.com>
2010-02-13 05:52:36 +0000

    New main scene

--- src/app/assistants/main-assistant.js
+++ src/app/assistants/main-assistant.js
@@ -1,59 +1,10 @@
 function MainAssistant(db) {
-	this.db = db;
-	this.podcastList_ctrl = undefined;
 }
 
 MainAssistant.prototype = {
 	setup: function() {
-	   Ares.setupSceneAssistant(this);
-	   
-	   // Add event listeners for the database
-	   this.db.addEventListener(PodcastStorage.PodcastStartUpdate, this.podcastUpdating.bind(this));
-	   this.db.addEventListener(PodcastStorage.PodcastUpdateSuccess, this.podcastUpdateSuccess.bind(this));
-	   this.db.addEventListener(PodcastStorage.PodcastUpdateFailure, this.podcastUpdateFailure.bind(this));
-	   
-	   // Get reference to the list on the screen
-	   this.podcastList_ctrl = this.controller.get('podcastList');
-	   
-	   // Populate the list with the podcasts
-	   this.podcastListModel.items = this.db.getPodcastList();
-	   this.controller.modelChanged(this.podcastListModel);
 	},
 	cleanup: function() {
 		Ares.cleanupSceneAssistant(this);
-	},
-	activate: function() {
-	   this.db.updatePodcasts();
 	}
-};
-
-// Event listener that is passed the key to the podcast that is
-// about to update
-MainAssistant.prototype.podcastUpdating = function(podcastKey) {
-	Mojo.Log.info("[MainAssistant] Podcast %s updating.", podcastKey);
-	var spinnerAttr = {
-	   spinnerSize: Mojo.Widget.spinnerSmall
-	};
-	
-	var spinnerModel = {
-	   spinning: true
-	};
-	
-	// Turn on the spinner
-	this.controller.setupWidget($("spinner_"+podcastKey), spinnerAttr, spinnerModel);
-};
-
-// Event listener that is passed the key to the podcast that
-// has successfully updated
-MainAssistant.prototype.podcastUpdateSuccess = function(podcastKey) {
-	Mojo.Log.info("[MainAssistant] Podcast %s finished updating.", podcastKey);
-	// Populate the list with the podcasts
-	this.podcastListModel.items = this.db.getPodcastList();
-	this.controller.modelChanged(this.podcastListModel);
-};
-
-// Event listener that is passed the key to the podcast that
-// has not successfully updated
-MainAssistant.prototype.podcastUpdateFailure = function(podcastKey) {
-	
 };
--- src/app/assistants/stage-assistant.js
+++ src/app/assistants/stage-assistant.js
@@ -1,6 +1,4 @@
 function StageAssistant() {
-	this.db = new PodcastStorage();
-	this.db.addEventListener(PodcastStorage.LoadingDatabaseSuccess, this.podcastListFilled.bind(this));
 }
 
 StageAssistant.prototype.setup = function() {
@@ -9,14 +7,6 @@ StageAssistant.prototype.setup = function() {
 	} catch(version_error) {
 		Mojo.Log.error("[Prototype] %s", version_error.message);
 	}
-};
-
-StageAssistant.prototype.podcastListFilled = function() {
-	Mojo.Log.info("[StageAssistant] Podcast list updated.");
-	// The stage no longer needs to listen for updates to the database
-	this.db.removeEventListener(PodcastStorage.LoadingDatabaseSuccess, this.podcastListFilled.bind(this));
-	
-	// Start the scene
-	this.controller.pushScene({name: "main", disableSceneScroller: true}, this.db);
+	this.controller.pushScene({name: "main", disableSceneScroller: true});
 	this.controller.setWindowOrientation("free");
 };
