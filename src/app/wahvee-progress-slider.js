190252ec65188a0498ff88f9deed2a58dc1e46b1
Ryan Lovelett <ryan@wahvee.com>
2010-03-26 00:57:42 +0000

    knights

--- src/app/wahvee-progress-slider.js
+++ src/app/wahvee-progress-slider.js
@@ -1,11 +1,37 @@
+/**
+ * Slider property defaults to "slider"
+ */
 Mojo.Widget.WahveeProgressSlider = Class.create({
-	initialize: function(test1, test2, test3) {
-		
+	initialize: function() {
 	},
-	setup: function(test1, test2, test3) {
+	setup: function() {
+		// The HTML element for this widget is found in...this.controller.element
+		// The name of the widget (or ID of the element that will become this widget) is...this.controller.widgetName
+		this.divPrefix = Mojo.View.makeUniqueId() + this.controller.scene.sceneId + this.controller.widgetName;
+		this.sliderValueProperty = this.controller.attributes.sliderProperty || "slider";
+		this.progressStartProperty = this.controller.attributes.progressStartProperty || "progressLowerValue";
+		this.progressEndProperty = this.controller.attributes.progressProperty || "progressUpperValue";
+		this.maxValue = this.controller.model.maxValue || 1;
+		this.minValue = this.controller.model.minValue || 0;
 		
+		// Now render the widget
+		this.renderWidget();
 	},
-	renderWidget: function(test1, test2, test3) {
+	renderWidget: function() {
+		var sliderTemplate = "wahvee-progress-slider/wahvee-progress-slider";
+		var physicalWidthOfSlider = this.controller.element.getWidth();
+		var physicalHeightOfSlider = this.controller.element.getHeight();
 		
+		var html = Mojo.View.render(
+			{
+				object: {
+					divPrefix: this.divPrefix
+				},
+				template: sliderTemplate
+			}
+		);
+		
+		// Add the generated HTML to the template
+		this.controller.element.update(html);
 	}
 });
