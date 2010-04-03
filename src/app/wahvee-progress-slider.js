8898084d6597f411e76e1056096f36d70824f980
Ryan Lovelett <ryan@wahvee.com>
2010-04-03 04:42:06 +0000

    Trying something else.

--- src/app/wahvee-progress-slider.js
+++ src/app/wahvee-progress-slider.js
@@ -3,6 +3,7 @@
  */
 Mojo.Widget.WahveeProgressSlider = Class.create({
 	initialize: function() {
+		this.seeking = false;
 		this.dragStartHandler = this.dragStartHandlerFunc.bindAsEventListener(this);
 		this.draggingHandler = this.draggingHandlerFunc.bindAsEventListener(this);
 	},
@@ -13,11 +14,18 @@ Mojo.Widget.WahveeProgressSlider = Class.create({
 		this.sliderValueProperty = this.controller.attributes.sliderProperty || "slider";
 		this.progressStartProperty = this.controller.attributes.progressStartProperty || "progressLowerValue";
 		this.progressEndProperty = this.controller.attributes.progressProperty || "progressUpperValue";
-		this.sliderMaxValue = this.controller.model.sliderMaxValue || 1;
-		this.sliderMinValue = this.controller.model.sliderMinValue || 0;
+		this.sliderMaxValue = this.controller.attributes.sliderMaxValue || 1;
+		this.sliderMinValue = this.controller.attributes.sliderMinValue || 0;
+		this.sliderValDifference = this.sliderMaxValue - this.sliderMinValue;
+		this.sliderPhysicalMax = 100;
+		this.sliderPhysicalMin = 0;
+		this.offset = 0;
 
 		// Now render the widget
 		this.renderWidget();
+
+		// Make the dragging area expandable
+		this.controller.exposeMethods(['updateDraggingArea']);
 	},
 	renderWidget: function() {
 		var sliderTemplate = "wahvee-progress-slider/wahvee-progress-slider";
@@ -36,6 +44,9 @@ Mojo.Widget.WahveeProgressSlider = Class.create({
 		this.progressBar = this.controller.get(this.divPrefix + "-progress");
 		this.slider = this.controller.get(this.divPrefix + "-slider-btn");
 
+		// Determine the offset to get the middle of the slider
+		this.offset = this.slider.getWidth() / 2;
+
 		// Start listening for dragging of the slider button
 		this.controller.listen(this.slider, Mojo.Event.dragStart, this.dragStartHandler);
 		this.controller.listen(this.slider, Mojo.Event.dragging, this.draggingHandler);
@@ -45,38 +56,45 @@ Mojo.Widget.WahveeProgressSlider = Class.create({
 		this.setDownloadPercentage();
 		this.setPositionSlider();
 	},
+	updateDraggingArea: function(newMin, newMax) {
+		this.sliderMaxValue = newMax;
+		this.sliderMinValue = newMin;
+		this.sliderValDifference = this.sliderMaxValue - this.sliderMinValue;
+	},
 	setPositionSlider: function() {
 		var position = this.controller.element.positionedOffset();
 		var physicalWidthOfSlider = this.controller.element.getWidth();
-		// Physical area able to allow sliding
-		this.sliderMaxValue = this.controller.model.sliderMaxValue || 1;
-		this.sliderMinValue = this.controller.model.sliderMinValue || 0;
 		var x1 = position.left;
 		var x2 = position.left + physicalWidthOfSlider;
-		var sliderValue = (this.controller.model[this.sliderValueProperty] >= this.sliderMinValue && this.controller.model[this.sliderValueProperty] <= this.sliderMaxValue) ? this.controller.model[this.sliderValueProperty] : this.sliderMaxValue;
-		var sliderXPos = (((x2 - x1) * (sliderValue - this.sliderMinValue)) / (this.sliderMaxValue - this.sliderMinValue)) + x1;
+		var sliderModelValue = this.controller.model[this.sliderValueProperty];
+		var sliderValue = (sliderModelValue >= this.sliderMinValue && sliderModelValue <= this.sliderMaxValue) ? sliderModelValue : this.sliderMaxValue;
+		var sliderXPos = (((x2 - x1) * (sliderValue - this.sliderMinValue)) / this.sliderValDifference) + x1;
 
 		this.slider.setStyle({
 			left: sliderXPos + "px"
 		});
 	},
 	setDownloadPercentage: function() {
-		var percentage = (this.controller.model[this.progressEndProperty] >= 0 && this.controller.model[this.progressEndProperty] <= 1) ? this.controller.model[this.progressEndProperty] : 1;
-		var lowPercent = (this.controller.model[this.progressStartProperty] >= 0 && this.controller.model[this.progressStartProperty] <= 1) ? this.controller.model[this.progressStartProperty] : 1;
+		var modelPercent = this.controller.model[this.progressEndProperty];
+		var modelLowPercent = this.controller.model[this.progressStartProperty];
+		var percentage = (modelPercent >= 0 && modelPercent <= 1) ? modelPercent : 1;
+		var lowPercent = (modelLowPercent >= 0 && modelLowPercent <= 1) ? modelLowPercent : 1;
 		this.progressBar.setStyle({
 			left: lowPercent * 100 + "%", // Lower bound of the percentage
 			width: percentage * 100 + "%" // Upper bound of the percentage
 		});
 	},
 	handleModelChanged: function() {
-		this.setDownloadPercentage();
-		this.setPositionSlider();
+		if(!this.seeking) {
+			this.setDownloadPercentage();
+			this.setPositionSlider();
+		}
 	},
 	/**
 	 * When the user starts trying to drag the slider-btn, make it draggable.
 	 */
 	dragStartHandlerFunc: function(event) {
-		//this.slider.addClassName("wahvee-progress-slider-brn-drag");
+		this.seeking = true;
 		var position = this.controller.element.positionedOffset();
 		var physicalWidthOfSlider = this.controller.element.getWidth();
 		Mojo.Drag.startDragging(this.controller.scene, this.slider, event.down, {
@@ -92,17 +110,16 @@ Mojo.Widget.WahveeProgressSlider = Class.create({
 	 * function called whenever the item moves over this container.
 	 */
 	draggingHandlerFunc: function(event) {
-		var offset = event.target.getWidth() / 2;
-		this.determineSliderValue(event.target.offsetLeft + offset);
+		//this.determineSliderValue(event.target.offsetLeft + this.offset);
 	},
 	/**
 	 * Called by the Mojo.Drag events.
 	 */
 	dragDrop: function(element) {
 		this.slider.removeClassName("wahvee-progress-slider-btn-drag");
-		var offset = element.getWidth() / 2;
-		this.determineSliderValue(element.offsetLeft + offset);
+		this.determineSliderValue(element.offsetLeft + this.offset);
 		Mojo.Event.send(this.controller.element, Mojo.Event.sliderDragEnd); //allow applications to see this event
+		this.seeking = false;
 	},
 	/**
 	 * Function is given an x position, this value must be greater than the minimum
@@ -113,13 +130,11 @@ Mojo.Widget.WahveeProgressSlider = Class.create({
 		var position = this.controller.element.positionedOffset();
 		var physicalWidthOfSlider = this.controller.element.getWidth();
 		// Physical area able to allow sliding
-		this.sliderMaxValue = this.controller.model.sliderMaxValue || 1;
-		this.sliderMinValue = this.controller.model.sliderMinValue || 0;
 		var x1 = position.left;
 		var x2 = position.left + physicalWidthOfSlider;
-		var sliderValue = (((this.sliderMaxValue - this.sliderMinValue) * (x - x1)) / (x2 - x1)) + this.sliderMinValue;
+		var sliderValue = ((this.sliderValDifference * (x - x1)) / (x2 - x1)) + this.sliderMinValue;
 		this.controller.model[this.sliderValueProperty] = sliderValue;
-		Mojo.Event.send(this.controller.element, Mojo.Event.propertyChange, {value: this.controller.model[this.sliderValueProperty]});
+		Mojo.Event.send(this.controller.element, Mojo.Event.propertyChange, {value: sliderValue});
 	},
 	cleanup: function() {
 		this.controller.stopListening(this.slider, Mojo.Event.dragStart, this.dragStartHandler);
