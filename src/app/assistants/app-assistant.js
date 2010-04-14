1fbf3c3a8694808a5a6c8a9107a75a0a19cd6f0e
Ryan Lovelett <ryan@wahvee.com>
2010-04-14 12:58:50 +0000

    Fix for error on quit.

--- src/app/assistants/app-assistant.js
+++ src/app/assistants/app-assistant.js
@@ -23,7 +23,7 @@ AppAssistant.prototype.setup = function(event) {
  */
 AppAssistant.prototype.cleanup = function() {
 	// Clean-up all of the event listeners for the HTML5 Audio object
-	this.controller.get("audio-element").stopObserving();
+	//this.controller.get("audio-element").stopObserving();
 };
 
 AppAssistant.prototype.handleLaunch = function(launchParams) {
