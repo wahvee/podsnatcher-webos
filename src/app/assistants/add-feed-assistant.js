9ae19f64f0c8abdb63a2bcc3fbb88d680771a1e4
Ryan Lovelett <ryan@wahvee.com>
2010-04-17 01:07:07 +0000

    Added the pop-up template and assistant for the UI additions for issue #9.

---
+++ src/app/assistants/add-feed-assistant.js
@@ -0,0 +1,49 @@
+function AddFeedAssistant(sceneAssistant) {
+	this.sceneAssistant = sceneAssistant;
+}
+
+AddFeedAssistant.prototype.setup = function(widget) {
+	this.widget = widget;
+
+	// Setup text field for the new feed's URL
+	//
+	this.sceneAssistant.controller.setupWidget("newFeedURL",
+		this.urlAttributes = {
+			property: "value",
+			hintText: "RSS or ATOM feed",
+			focus: true,
+			limitResize: true,
+			textReplacement: false,
+			enterSubmits: false
+		},
+		this.urlModel = {value : ""}
+	);
+
+	// Setup button and event handler
+	//
+	this.sceneAssistant.controller.setupWidget("okButton",
+		this.attributes = {},
+		this.model = {
+			buttonLabel: "Add Feed",
+			buttonClass: "addFeedButton",
+			disabled: false
+		}
+	);
+
+	Mojo.Event.listen($('okButton'), Mojo.Event.tap, this.validateAndAdd.bindAsEventListener(this));
+};
+
+// ------------------------------------------------------------------------
+// Add Feed Functions
+//
+// validateAndAdd - called when Add feed OK button is clicked
+//
+AddFeedAssistant.prototype.validateAndAdd = function() {
+	Mojo.Log.info("I'm here!");
+	var added = AppAssistant.db.addNewItem(this.urlModel.value, "", true, 0);
+	if(added) {
+		AppAssistant.db.updatePodcast();
+	} else {
+
+	}
+};
