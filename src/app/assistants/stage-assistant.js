48c01e7055a33c531dd9e2dc271140f4669d26fb
Lindsey Bortner <lindsey@wahvee.com>
2010-04-18 19:20:40 +0000

    issue #14, #13 Now Playing Audio Scene: white border, spacing issue, button and bar sizes. Quick Guide Scene: initial formatting.
    Fixes previous commit

--- src/app/assistants/stage-assistant.js
+++ src/app/assistants/stage-assistant.js
@@ -1,74 +1,3 @@
-<<<<<<< .mine
-function StageAssistant() {
-	AppAssistant.db = new PodcastStorage("podSnatcherDb");
-}
-
-StageAssistant.prototype.setup = function() {
-	this.controller.pushCommander(AppAssistant.db);
-
-	// Push the splash screen onto the stage
-	this.controller.pushScene({
-		name: "splash",
-		transition: Mojo.Transition.zoomFade
-	});
-};
-
-// handleCommand - Setup handlers for menus:
-StageAssistant.prototype.handleCommand = function(event) {
-	var currentScene = this.controller.activeScene();
-	switch(event.type) {
-		case PodcastStorage.LoadingDatabaseSuccess:
-			if(AppAssistant.db.listOfPodcasts.size() > 0) {
-				this.controller.swapScene({
-					name: "main",
-					transition: Mojo.Transition.zoomFade
-				});
-			} else {
-				this.controller.swapScene({
-					name: "add-remove",
-					transition: Mojo.Transition.zoomFade
-				});
-			}
-			break;
-		case Mojo.Event.commandEnable:
-			if(event.command === 'palm-help-cmd') {
-				event.stopPropagation();
-			}
-			break;
-		case Mojo.Event.command:
-			switch(event.command) {
-				case 'palm-prefs-cmd':
-					break;
-				case 'palm-help-cmd':
-					this.controller.pushAppSupportInfoScene();
-					break;
-				case 'do-refresh-all':
-					// Unload all scenes and then load the login scene
-					//this.controller.popScenesTo(undefined, undefined, undefined);
-					break;
-				case 'do-add-remove':
-					// Start the add remove buttons
-					this.controller.pushScene({
-						name: "add-remove",
-						transition: Mojo.Transition.zoomFade
-					});
-					break;
-				case 'quick-guide':
-					this.controller.pushScene({
-						name: "quick-guide",
-						transition: Mojo.Transition.zoomFade
-					});
-				default:
-					Mojo.Log.error("There was an un-recognized app menu command. %s", event.command);
-					break;
-			}
-			break;
-		default:
-			Mojo.Log.info("[StageAssistant.handleCommand] Not handling %s", event.type);
-			break;
-	}
-};
-=======
 function StageAssistant() {
 	AppAssistant.db = new PodcastStorage("podSnatcherDb");
 }
@@ -125,6 +54,11 @@ StageAssistant.prototype.handleCommand = function(event) {
 						transition: Mojo.Transition.zoomFade
 					});
 					break;
+				case 'quick-guide':
+					this.controller.pushScene({
+						name: "quick-guide",
+						transition: Mojo.Transition.zoomFade
+					});
 				default:
 					Mojo.Log.error("There was an un-recognized app menu command. %s", event.command);
 					break;
@@ -135,4 +69,3 @@ StageAssistant.prototype.handleCommand = function(event) {
 			break;
 	}
 };
->>>>>>> .r565
